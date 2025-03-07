a5a7720f41216b20410916e0c7ba344a419bdf63
Global Medical Devices <globalmedicaldevices@Globals-MacBook-Air.local>
2025-03-07 21:19:45 +0530

    Initial commit

---
+++ KridaShastra/urls.py
@@ -0,0 +1,47 @@
+"""
+URL configuration for KridaShastra project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myapp import views  # Import views from your app
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # Home Page
+    path('', views.index, name='index'),
+    
+    # About Page
+    path('about/', views.about, name='about'),
+    
+    # Services Page
+    path('services/', views.services, name='services'),
+    
+    # Networking Page
+    path('networking/', views.networking, name='networking'),
+    
+    # Insights Page
+    path('insights/', views.insights, name='insights'),
+    
+    # Resources Page
+    path('resources/', views.resources, name='resources'),
+    
+    # Testimonials Page
+    path('testimonials/', views.testimonials, name='testimonials'),
+    
+    # Contact Page
+    path('contact/', views.contact, name='contact'),
+]
---
+++ myapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def services(request):
+    return render(request, 'main/services.html')
+
+def networking(request):
+    return render(request, 'main/networking.html')
+
+def insights(request):
+    return render(request, 'main/insights.html')
+
+def resources(request):
+    return render(request, 'main/resources.html')
+
+def testimonials(request):
+    return render(request, 'main/testimonials.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
