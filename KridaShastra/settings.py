4e23f51899c3e1d21d0a9a817809a795a36950a7
Global Medical Devices <globalmedicaldevices@MacBookAir.lan>
2025-04-07 17:05:31 +0530

    Live Update2

--- KridaShastra/settings.py
+++ KridaShastra/settings.py
@@ -118,13 +118,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = '/static/'  # 👈 Add this line
+if DEBUG == False:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = '/KridaS/KridaShastra/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'staticfiles')
+    ]
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = 'staticfiles/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
 
-# Optional if you use custom static folders:
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
